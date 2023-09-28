5c7b3f04df771288ab9390628a4d531c6607a90b
vincecloutier <vincentcloutier@mail.com>
2023-09-28 16:14:06 -0400

    ready for feedback

--- covid-consumption-graph.py
+++ covid-consumption-graph.py
@@ -2,7 +2,7 @@ from manim import *
 
 class ConsumptionGraph(Scene):
     def construct(self):
-        # create graph
+        # Create graph
         axes = Axes(x_range=[0, 10], y_range=[0, 10], tips=False, axis_config={"color": BLUE}).shift(DOWN * 0.5)
         labels = axes.get_axis_labels(x_label="Y-T", y_label="C")
 
@@ -15,21 +15,23 @@ class ConsumptionGraph(Scene):
         consumption_line = axes.plot(pre_consumption_func, color=WHITE)
         limit_line = axes.plot(lambda x: 5, color=RED)
 
-
         title = Tex("Consumption before COVID").to_edge(UP)
 
-        # first scene
-        self.play(Create(axes), Create(labels), Write(title), Create(consumption_line))
-        self.wait(3)
+        # First scene
+        self.play(Create(axes), Create(labels), Write(title), Create(consumption_line), run_time=2)
+        self.wait(2)
 
-        # second scene
-        self.play(Transform(title, Tex("Consumption during COVID").to_edge(UP)))
+        # Second scene
+        self.play(Transform(title, Tex("Consumption during COVID").to_edge(UP)), run_time=1)
         self.wait(0.5)
-        self.play(Create(limit_line), Transform(consumption_line, axes.plot(during_consumption_func, color=YELLOW)))
-        self.wait(3)
+        self.play(Create(limit_line), Transform(consumption_line, axes.plot(during_consumption_func, color=YELLOW)), run_time=2)
+        self.wait(2)
 
-        # third scene
-        self.play(Transform(title, Tex("Consumption after COVID").to_edge(UP)), FadeOut(limit_line))
+        # Third scene
+        self.play(Transform(title, Tex("Consumption after COVID").to_edge(UP)), FadeOut(limit_line), run_time=1)
         self.wait(0.5)
-        self.play(Transform(consumption_line, Line(axes.c2p(0,1), axes.c2p(4.5,10), color=GREEN)))
-        self.wait(3)
+        self.play(Transform(consumption_line, Line(axes.c2p(0,1), axes.c2p(4.5,10), color=GREEN)), run_time=2)
+        self.wait(2)
+                
+        # Cleanup
+        self.play(FadeOut(consumption_line), FadeOut(title), FadeOut(labels), FadeOut(axes), run_time=1.5)
