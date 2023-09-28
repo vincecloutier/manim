06f7dff225c986f9a9f81c06440e26f90153d4d9
vincecloutier <vincentcloutier@mail.com>
2023-09-28 16:07:07 -0400

    better

--- covid-consumption-graph.py
+++ covid-consumption-graph.py
@@ -2,44 +2,39 @@ from manim import *
 
 class ConsumptionGraph(Scene):
     def construct(self):
-        axes = Axes(
-            x_range=[0, 10],
-            y_range=[0, 10],
-            axis_config={"color": BLUE},
-        )
-
-        # Labels
+        # create graph
+        axes = Axes(x_range=[0, 10], y_range=[0, 10], tips=False, axis_config={"color": BLUE})
         labels = axes.get_axis_labels(x_label="Y-T", y_label="C")
 
         def pre_consumption_func(x):
-            return x
+            return min(x, 10)
         
-        consumption_line = axes.plot(pre_consumption_func, color=WHITE)
-
-        # Title
-        title = Tex("Consumption before COVID").to_edge(UP)
-
-        self.play(
-            Create(axes),
-            Create(labels),
-            Create(consumption_line),
-            Write(title)
-        )
-        self.wait()
-
-        # Limit line and label
-        limit_line = axes.plot(lambda x: 5, color=RED)
-
-        self.play(
-            Transform(title, Tex("Consumption during COVID").to_edge(UP)),
-            Create(limit_line),)
-        self.wait()
+        def during_consumption_func(x):
+            return min(x, 5) # Cap at C = 5
 
         def post_consumption_func(x):
-            return 1 + 2 * x
-        
-        self.play(
-            Transform(consumption_line, axes.plot(post_consumption_func, color=YELLOW)),
-            Transform(title, Tex("Consumption after COVID").to_edge(UP))
-        )
-        self.wait()
+            y = 1 + 2 * x
+            return y if y <= 10 else 10  # Adjusted to stop at Y = 10
+
+                
+        consumption_line = axes.plot(pre_consumption_func, color=WHITE)
+        limit_line = axes.plot(lambda x: 5, color=RED)
+
+
+        title = Tex("Consumption before COVID").to_edge(UP)
+
+        # first scene
+        self.play(Create(axes), Create(labels), Write(title), Create(consumption_line))
+        self.wait(3)
+
+        # second scene
+        self.play(Transform(title, Tex("Consumption during COVID").to_edge(UP)))
+        self.wait(0.5)
+        self.play(Create(limit_line), Transform(consumption_line, axes.plot(during_consumption_func, color=YELLOW)))
+        self.wait(3)
+
+        # third scene
+        self.play(Transform(title, Tex("Consumption after COVID").to_edge(UP)), FadeOut(limit_line))
+        self.wait(0.5)
+        self.play(Transform(consumption_line, Line(axes.c2p(0,1), axes.c2p(4.5,10), color=GREEN)))
+        self.wait(3)
